282b549a852a8adfc87ecad96b94c5308e882b85
Alekseypavlov14 <aleshapavlov14@gmail.com>
2021-12-28 21:44:19 +0200

    fix bags and finish mobile home, destinations and crew

--- starter-code/src/styles/home/burger.style.js
+++ starter-code/src/styles/home/burger.style.js
@@ -1,9 +1,12 @@
 const close_btn = document.querySelector('.close-btn img');
 const burger_btn = document.querySelector('.burger-btn');
 const menu = document.querySelector('.burger-wrap');
+const body = document.querySelector('body')
 
 burger_btn.onclick = (e) => {
+    const height = body.scrollHeight
     e.preventDefault()
+    menu.style.height = height + 'px'
     menu.style.width = '70%' 
 }
 
