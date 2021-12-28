8f3409ab9cc5b22d4cb73a70672a48b88978f967
Alekseypavlov14 <aleshapavlov14@gmail.com>
2021-12-28 18:37:18 +0200

    crew is changed

--- starter-code/src/styles/crew/crew.style.js
+++ starter-code/src/styles/crew/crew.style.js
@@ -1,25 +1,37 @@
-const link1 = document.querySelector('.link1 .nav-link-text')
-const link2 = document.querySelector('.link2 .nav-link-text')
-const link3 = document.querySelector('.link3 .nav-link-text')
-const link4 = document.querySelector('.link4 .nav-link-text')
+const link1 = document.querySelector('.link1')
+const link2 = document.querySelector('.link2')
+const link3 = document.querySelector('.link3')
+const link4 = document.querySelector('.link4')
+const circles = document.querySelectorAll('.nav-link')
 
-const name = document.querySelector('.main-article-title')
+const person = document.querySelector('.main-article-title')
 const image = document.querySelector('#img')
-const role = document.querySelector('main-article-pre-title')
-const bio = document.querySelector('main-article-content')
+const role = document.querySelector('.main-article-pre-title')
+const bio = document.querySelector('.main-article-content')
 
 const links = [link1, link2, link3, link4]
 
-links.forEach((el, index) => {
-    el.onclick = (e) => {
-        e.preventDefault()
-        
-        fetch('data.json').then((res) => {
-            data = res.json()
-            name.innerHTML = data['crew'][i]['name']
-            bio.innerHTML = data['crew'][i]['bio']
-            role.innerHTML = data['crew'][i]['role']
-            image.src = data['crew'][i]['images']['png']
+links.forEach((el, i) => {
+    el.onclick = (event) => {
+        event.preventDefault()
+    
+        fetch('http://127.0.0.1:5500/starter-code/data.json').then((res) => {
+            return Promise.resolve(res)
+        }).then(response => {
+            return response.json()
+        }).then(data => {
+            return data.crew
+        }).then(data => {
+            person.innerText = data[i].name;
+            bio.innerText = data[i].bio;
+            role.innerText = data[i].role;
+            image.src = data[i].images.png;
+
+            circles.forEach(element => {
+                element.classList.remove('link-active')
+            })
+
+            circles[i].classList.add('link-active')
         })
     }
 })
