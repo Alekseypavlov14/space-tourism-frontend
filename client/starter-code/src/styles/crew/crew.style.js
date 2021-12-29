1786ccb021efa151ea06d7d1ff5ded39be322990
Alekseypavlov14 <aleshapavlov14@gmail.com>
2021-12-29 11:13:48 +0200

    server is created

---
+++ client/starter-code/src/styles/crew/crew.style.js
@@ -0,0 +1,37 @@
+const link1 = document.querySelector('.link1')
+const link2 = document.querySelector('.link2')
+const link3 = document.querySelector('.link3')
+const link4 = document.querySelector('.link4')
+const circles = document.querySelectorAll('.nav-link')
+
+const person = document.querySelector('.main-article-title')
+const image = document.querySelector('#img')
+const role = document.querySelector('.main-article-pre-title')
+const bio = document.querySelector('.main-article-content')
+
+const links = [link1, link2, link3, link4]
+
+links.forEach((el, i) => {
+    el.onclick = (event) => {
+        event.preventDefault()
+    
+        fetch('http://127.0.0.1:5500/client/starter-code/data.json').then((res) => {
+            return Promise.resolve(res)
+        }).then(response => {
+            return response.json()
+        }).then(data => {
+            return data.crew
+        }).then(data => {
+            person.innerText = data[i].name;
+            bio.innerText = data[i].bio;
+            role.innerText = data[i].role;
+            image.src = data[i].images.png;
+
+            circles.forEach(element => {
+                element.classList.remove('link-active')
+            })
+
+            circles[i].classList.add('link-active')
+        })
+    }
+})
---
+++ index.js
@@ -0,0 +1,22 @@
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+const server = http.createServer((req, res) => {
+
+    let filePath = path.join(__dirname, 'client', 'starter-code' + req.url)
+    if (req.url === '/') filePath += 'index.html'
+    if (req.url === '/destination.html') {}
+
+    fs.readFile(filePath, (e, data) => {
+        if (e) {
+            try{
+                console.error(e)
+            } catch{}
+        }
+
+        res.end(data)
+    })
+})
+
+server.listen(3000, () => console.log("Server is working"))
